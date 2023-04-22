46b67f696ba185648a4c00ad3a954af9e3e0a52c
Rahul Kumar <connectrahul25@gmail.com>
2023-04-22 19:42:32 +0530

    22-04-2023 : DB Connection & Server Initilization

--- server/api/utility/dbConnection.js
+++ server/api/utility/dbConnection.js
@@ -1,1 +1,20 @@
 //Mongo DB Connection 
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+    const connection = async (DB_URL)=>{
+
+    try{
+       await mongoose.connect(DB_URL,{useNewUrlParser:true});
+       console.log("🍀 Database Connected successfully!");
+
+    }
+     catch(error){
+        console.log("😢 Error connecting to database!!!");
+        console.log(error);
+    }
+}
+
+export default connection;
--- server/server.js
+++ server/server.js
@@ -3,15 +3,22 @@ import express from 'express';
 import  dotenv  from "dotenv";
 
 //Importing Necessary Files
-import connection from './api/utility/dbconnection.js';
+import connection from './api/utility/dbConnection.js';
 
 const app = express();
 dotenv.config();
 
+//Environment Variables
 const PORT = process.env.PORT;
-const DB_URL = process.env.DBURL;
+const DB_URL = process.env.DBURI;
 
+//Listening to Server
 app.listen(PORT,() => console.log(`Server running on port: http://localhost:${PORT}`))
+
+//Home Route
+app.get('/',(req,res) => {
+    res.send("Home Page!");
+});
 
 //Connection to MongoDB Database
 connection(DB_URL);
