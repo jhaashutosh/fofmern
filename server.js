c087fa88fa0864727be35b4fbc4ea53d30e6cced
Rahul Kumar <connectrahul25@gmail.com>
2023-04-22 19:22:17 +0530

    22-04-2023 : Initializing Client Side

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-
+//Importing Necessary Packages
 import express from 'express';
-
-import connection from './api/utility/dbconnection.js';
-
 import  dotenv  from "dotenv";
+
+//Importing Necessary Files
+import connection from './api/utility/dbconnection.js';
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT;
+const DB_URL = process.env.DBURL;
 
 app.listen(PORT,() => console.log(`Server running on port: http://localhost:${PORT}`))
 
-const DB_URL = process.env.DBURL;
-
+//Connection to MongoDB Database
 connection(DB_URL);
