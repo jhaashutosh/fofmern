159e7564b9ce51704f19ef83c62a3f76901f1667
ashutosh <jhaashutosh58@gmail.com>
2023-04-24 22:19:59 +0530

    24-04-2023: auth middleware setup and testing performed.

--- server/api/routes/authRoutes.js
+++ server/api/routes/authRoutes.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { authVerifyMiddleware } = require("../middlewares/authVerifyMiddleware");
 
-const {signupController,verifyMail,loginController} = require("../controllers/authController");
-const {signupValidator} = require("../functions/signupValidator");
+const {
+  signupController,
+  verifyMail,
+  loginController,
+} = require("../controllers/authController");
+const { signupValidator } = require("../functions/signupValidator");
 
 router.post("/signup", signupValidator, signupController);
 
-router.post("/login",loginController);
+router.post("/login", loginController);
+
+//testing the middleware
+router.get("/middleware", authVerifyMiddleware, () => {
+  res.send("middleware testing");
+});
 
 router.get("/verify", verifyMail);
 
