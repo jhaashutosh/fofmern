453d5f9c5778574002aa49866ac030345a5c7340
Rahul Kumar <connectrahul25@gmail.com>
2023-04-28 16:41:48 +0530

    28-04-2023 : allDetailsValidator

--- server/api/routes/authRoutes.js
+++ server/api/routes/authRoutes.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { authVerifyMiddleware } = require("../middlewares/authVerifyMiddleware");
 
-const { signupController, verifyMail, loginController} = require("../controllers/authController");
+const { signupController, verifyMail, loginController, allDetailsController} = require("../controllers/authController");
+
 const { signupValidator } = require("../functions/signupValidator");
 const { loginValidator } = require("../functions/loginValidator");
+const { allDetailsValidator } = require("../functions/allDetailsValidator");
 
 router.post("/signup", signupValidator, signupController);
 
 router.post("/login", loginValidator, loginController);
+
+router.post("/allDetails", allDetailsValidator, allDetailsController);
 
 //testing the middleware
 router.get("/middleware", authVerifyMiddleware, () => {
